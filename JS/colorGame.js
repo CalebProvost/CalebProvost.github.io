a5be6f6a9cd64fc0d391d6e4911045f67d373df1
CalebProvost <dhx664@gmail.com>
2019-06-28 17:57:52 -0400

    updated hosted files with actual content, no more dead links

--- JS/colorGame.js
+++ JS/colorGame.js
@@ -4,40 +4,48 @@ const hardDifficulty = document.getElementById("hardDifficulty");
 const customDifficulty = document.getElementById("customDifficulty");
 const customButton = document.getElementById("customButton");
 
+const score = document.querySelector("span.score");
+score.textContent = 0;
+
 easyDifficulty.addEventListener('click', () => {function addRow() {
-  // Get a reference to the table
-  var tableRef = document.getElementById('autoTable');
+    // Get a reference to the table
+    var tableRef = document.getElementById('autoTable');
 
-    //delete rows before displaying new difficulty
-  for(var i = tableRef.rows.length - 1; i > -1; i--)
-  {
-    tableRef.deleteRow(i);
-  }
-  
-  for(var k=0;k<5;k++){
-    // Insert a row in the table at row index 0
-    var newRow = tableRef.insertRow(-1);
+      //delete rows before displaying new difficulty
+    for(var i = tableRef.rows.length - 1; i > -1; i--)
+    {
+      tableRef.deleteRow(i);
+    }
+    
+    for(var k=0;k<5;k++){
+      // Insert a row in the table at row index 0
+      var newRow = tableRef.insertRow(-1);
+      newRow.className = k;
 
-    // Insert a cell in the row at index 0
-    for(var j =0; j<5;j++){
-      var newCell = newRow.insertCell(j);
+      // Insert a cell in the row at index 0
+      for(var j =0; j<5;j++){
+        var newCell = newRow.insertCell(j);
+        newCell.className = j;
+      }
     }
   }
-}
+  
+  //inner function variables
 
-  function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-  }
 
-  addRow();
-  $('#autoTable').each(function () {
-      $(this).css('background-color', getRandomColor());
-    });
+    function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+    }
+
+    addRow();
+    $('#autoTable').each(function () {
+        $(this).css('background-color', getRandomColor());
+      });
 });
 
 
@@ -53,10 +61,12 @@ mediumDifficulty.addEventListener('click', () => {function addRow() {
   for(var k=0;k<10;k++){
     // Insert a row in the table at row index 0
     var newRow = tableRef.insertRow(-1);
+    newRow.className = k;
 
     // Insert a cell in the row at index 0
     for(var j =0; j<10;j++){
     var newCell = newRow.insertCell(j);
+    newCell.className = j;
   }
  }
 }
@@ -100,10 +110,12 @@ hardDifficulty.addEventListener('click', () => {function addRow() {
   for(var k=0;k<15;k++){
     // Insert a row in the table at row index 0
     var newRow = tableRef.insertRow(-1);
+    newRow.className = k;
 
     // Insert a cell in the row at index 0
     for(var j =0; j<15;j++){
     var newCell = newRow.insertCell(j);
+    newCell.className = j;
   }
  }
 }
@@ -147,10 +159,12 @@ customButton.addEventListener('click', () => {function addRow() {
   for(var k=0;k<customDifficulty.value;k++){
     // Insert a row in the table at row index 0
     var newRow = tableRef.insertRow(-1);
+    newRow.className = k;
 
     // Insert a cell in the row at index 0
     for(var j =0; j<customDifficulty.value;j++){
     var newCell = newRow.insertCell(j);
+    newCell.className = j;
   }
  }
 }
